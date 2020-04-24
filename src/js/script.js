78984cde2018cbe1cc8befef9d982410d2f07d33
Elizabeth <ekcarney@smith.edu>
2020-04-24 15:15:09 -0400

    initialize files

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+
+/*
+<script type="text/javascript">
+    $("document").ready(function(){
+
+      $('.svgload').on('click', function() {
+          alert('clicked svg new');
+      }).svg({loadURL: '/img/new.svg'});
+
+    });
+</script>
+*/
